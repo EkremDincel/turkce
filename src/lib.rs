4f2744eb8bea2a11f1ec9fa5e45d5cb4965edfa0
EkremDincel <ekremdincel@gmail.com>
2020-11-24 18:27:23 +0300

    Added WordController struct.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,53 @@
+use std::collections::HashSet;
+use std::str::CharIndices;
+
+pub struct WordController<'a> {
+	string: &'a String,
+	char_iter: CharIndices<'a>,
+	last: usize,
+	curr: usize,
+}
+
+impl<'a> WordController<'a> {
+	pub fn new(string: &String) -> WordController {
+		WordController {
+			string: string,
+			char_iter: string.char_indices(),
+			last: 0,
+			curr: 0,
+		}
+	}
+
+	pub fn take_part(&mut self) -> &str {
+		let s = unsafe {
+			self.string.get_unchecked(self.last..self.curr)
+		};
+		self.last = self.curr;
+
+		s
+	}
+}
+
+impl<'a> Iterator for WordController<'a> {
+	type Item = char;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		if let Some((i, j)) = self.char_iter.next() {
+			self.curr = i;
+			Some(j)
+		} else {
+			None
+		}
+	}
+}
+
+
 #[cfg(test)]
 mod tests {
+	use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn check_take_part() {
+
     }
 }
