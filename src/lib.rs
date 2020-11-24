74403b3d5d920efa4e9f33495ae7d0fc4eca2230
EkremDincel <ekremdincel@gmail.com>
2020-11-24 21:36:22 +0300

    Updated lib.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 use std::collections::HashSet;
 use std::str::CharIndices;
 
-pub struct WordController<'a> {
+pub struct StringSlicer<'a> {
 	string: &'a String,
 	char_iter: CharIndices<'a>,
 	last: usize,
 	curr: usize,
 }
 
-impl<'a> WordController<'a> {
-	pub fn new(string: &String) -> WordController {
-		WordController {
+impl<'a> StringSlicer<'a> {
+	pub fn new(string: &String) -> StringSlicer {
+		StringSlicer {
 			string: string,
 			char_iter: string.char_indices(),
 			last: 0,
@@ -19,16 +19,21 @@ impl<'a> WordController<'a> {
 	}
 
 	pub fn take_part(&mut self) -> &str {
-		let s = unsafe {
-			self.string.get_unchecked(self.last..self.curr)
-		};
+		let s = unsafe { self.string.get_unchecked(self.last..self.curr) };
+		self.last = self.curr;
+
+		s
+	}
+
+	pub fn take_last_part(&mut self) -> &str {
+		let s = unsafe { self.string.get_unchecked(self.last..) };
 		self.last = self.curr;
 
 		s
 	}
 }
 
-impl<'a> Iterator for WordController<'a> {
+impl<'a> Iterator for StringSlicer<'a> {
 	type Item = char;
 
 	fn next(&mut self) -> Option<Self::Item> {
@@ -47,7 +52,19 @@ mod tests {
 	use super::*;
 
     #[test]
-    fn check_take_part() {
+    fn test_string_slicer_take_part() {
+      	let s = "merhaba dünya".to_string();
+        let mut w = StringSlicer::new(&s);
 
+        let t = ["merh", "ab", "a düny", "a"];
+        let mut j = 0;
+        // seet the bug at: https://github.com/rust-lang/rust/issues/8372
+        while let Some(i) = w.next() {
+        	if i == 'a' {
+        		assert_eq!(t[j], w.take_part());
+        		j+=1;
+        	}
+        }
+        assert_eq!(t[j], w.take_last_part());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
 use turkce::*;
 
 fn main() {
-    	let s = "merhaba dünya".to_string();
-        let mut w = WordController::new(&s);
-        while let Some(i) = w.next() {
-        	if i == 'a' {
-        		println!("{}", w.take_part());
-        	}
-        }
+
 }
